e1bd6ac6be9185136d96c67e56745cf0586597e9
Vladan Tomić <tomic.vladan.mail@gmail.com>
2023-11-06 17:07:18 +0100

    feat: adding comments

---
+++ src/components/swarm-comment-system/swarm-comment-form/swarm-comment-form.tsx
@@ -0,0 +1,74 @@
+import { CommentRequest } from "@ethersphere/comment-system";
+import styles from "./swarm-comment-form.module.scss";
+import { useState } from "react";
+
+export interface SwarmCommentFormProps {
+  loading: boolean;
+  onSubmit: (comment: CommentRequest) => void;
+}
+
+interface FormElements extends HTMLFormControlsCollection {
+  user: HTMLInputElement;
+  data: HTMLInputElement;
+}
+interface CommentFormElement extends HTMLFormElement {
+  readonly elements: FormElements;
+}
+
+interface FormErrors {
+  user?: string;
+  data?: string;
+}
+
+export default function SwarmCommentForm({
+  loading,
+  onSubmit,
+}: SwarmCommentFormProps) {
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const validate = (value: string): string | undefined => {
+    if (!value) {
+      return "This field is required.";
+    }
+  };
+  const hasErrors = (errors: FormErrors): boolean => {
+    return Object.values(errors).some((value) => Boolean(value));
+  };
+
+  const submit = (event: React.FormEvent<CommentFormElement>) => {
+    event.preventDefault();
+    const elements = event.currentTarget.elements;
+    const user = elements.user.value;
+    const data = elements.data.value;
+    const errors: FormErrors = {
+      user: validate(user),
+      data: validate(data),
+    };
+
+    if (hasErrors(errors)) {
+      return setErrors(errors);
+    }
+
+    onSubmit({ user, data });
+  };
+
+  return (
+    <form className={styles["swarm-comment-form"]} onSubmit={submit}>
+      <input
+        className={errors.user && styles["field-error"]}
+        onChange={() => setErrors({ ...errors, user: undefined })}
+        type="text"
+        name="user"
+        placeholder="Your name"
+        disabled={loading}
+      />
+      <textarea
+        className={errors.data && styles["field-error"]}
+        onChange={() => setErrors({ ...errors, data: undefined })}
+        name="data"
+        disabled={loading}
+      ></textarea>
+      <button disabled={loading}>Submit</button>
+    </form>
+  );
+}
--- src/components/swarm-comment-system/swarm-comment-system.tsx
+++ src/components/swarm-comment-system/swarm-comment-system.tsx
@@ -1,6 +1,12 @@
-import { Comment, readComments } from "@ethersphere/comment-system";
+import {
+  Comment,
+  CommentRequest,
+  readComments,
+  writeComment,
+} from "@ethersphere/comment-system";
 import SwarmCommentList from "./swarm-comment-list/swarm-comment-list";
 import { useEffect, useState } from "react";
+import SwarmCommentForm from "./swarm-comment-form/swarm-comment-form";
 
 /**
  * stamp - Postage stamp ID. If ommitted a first available stamp will be used.
@@ -18,12 +24,31 @@ export interface SwarmCommentSystemProps {
 export default function SwarmCommentSystem(props: SwarmCommentSystemProps) {
   const [comments, setComments] = useState<Comment[] | null>(null);
   const [loading, setLoading] = useState(true);
+  const [formLoading, setFormLoading] = useState(false);
 
   const loadComments = async () => {
     const comments = await readComments(props);
 
     setComments(comments);
     setLoading(false);
+  };
+
+  const sendComment = async (comment: CommentRequest) => {
+    try {
+      setFormLoading(true);
+
+      await writeComment(comment, props);
+
+      setComments([
+        ...(comments as Comment[]),
+        { ...comment, timestamp: new Date().getTime() },
+      ]);
+    } catch (error) {
+      // TODO the error should be displayed on page
+      alert(error);
+    } finally {
+      setFormLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -38,5 +63,10 @@ export default function SwarmCommentSystem(props: SwarmCommentSystemProps) {
     return <div>Couldn't load comments</div>;
   }
 
-  return <SwarmCommentList comments={comments} />;
+  return (
+    <div>
+      <SwarmCommentForm onSubmit={sendComment} loading={formLoading} />
+      <SwarmCommentList comments={comments} />
+    </div>
+  );
 }
